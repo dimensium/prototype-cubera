19a9232cf0eb772f10fe648556ae48da099c22c8
Promesis <M3149154722@outlook.com>
2024-04-16 09:49:34 +0000

    test code upload

--- lib/prototype_cubera.dart
+++ lib/prototype_cubera.dart
@@ -1,3 +1,11 @@
+import "dart:io";
+
 int calculate() {
   return 6 * 7;
+}
+
+Future<String> getFileContents(String filename) async {
+  var fileObject = File(filename);
+  var content = await fileObject.readAsString();
+  return content;
 }
--- test/prototype_cubera_test.dart
+++ test/prototype_cubera_test.dart
@@ -1,8 +1,18 @@
 import 'package:prototype_cubera/prototype_cubera.dart';
 import 'package:test/test.dart';
+import "dart:io";
 
 void main() {
   test('calculate', () {
     expect(calculate(), 42);
+  });
+
+  test("getFileContents", () async{
+    {
+      // prepare file data.
+      var testFile = File("testSource.txt");
+      testFile.writeAsString("hello");
+    }
+    expect(await getFileContents("testSource.txt"), "hello");
   });
 }
